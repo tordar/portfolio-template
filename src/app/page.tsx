746706828b96af9e073b47bf5cf257e179ba337e
Tordar Tømmervik <tordar.tommervik@gmail.com>
2024-11-26 10:28:34 +0100

    remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import AboutMe from '../components/AboutMe'
-import TimeThemeSwitcher from "@/src/components/TimeTheme";
 
 interface Project {
     title: string;
